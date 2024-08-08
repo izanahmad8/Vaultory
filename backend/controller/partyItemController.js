7ff3ecd7598be36d30b11ab1081dbcba96123238
Izan Ahmad <izan.khan596@gmail.com>
2024-08-08 20:30:19 +0530

    updateItem

--- backend/controller/partyItemController.js
+++ backend/controller/partyItemController.js
@@ -56,7 +56,51 @@ const addPartyItem = async (req, res) => {
 
 //update partyItem
 
-const updatePartyItem = async (req, res) => {};
+const updatePartyItem = async (req, res) => {
+  const { id } = req.params;
+  const { itemName, size, price, sellPrice, quantity, discount, date } =
+    req.body;
+  const { error } = partyItemSchema.validate({
+    itemName,
+    size,
+    price,
+    sellPrice,
+    quantity,
+    discount,
+    date,
+  });
+
+  if (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+
+  try {
+    const partyItem = await partyItemModel.findById(id);
+    if (!partyItem) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Item not found" });
+    }
+
+    partyItem.itemName = itemName;
+    partyItem.size = size;
+    partyItem.price = price;
+    partyItem.sellPrice = sellPrice;
+    partyItem.quantity = quantity;
+    partyItem.discount = discount;
+    partyItem.date = date;
+
+    const updateItem = await partyItem.save();
+    res.status(200).json({
+      success: true,
+      message: "Item updated successfully",
+      partyItem: updateItem,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
 
 //delete partyItem
 
