529a304f4b3a7526200263482537c0b94f86230b
Izan Ahmad <izan.khan596@gmail.com>
2024-08-08 20:19:00 +0530

    additem

--- backend/controller/partyItemController.js
+++ backend/controller/partyItemController.js
@@ -3,7 +3,56 @@ import { partyItemSchema } from "../config/authValidation.js";
 
 //add partyItem
 
-const addPartyItem = async (req, res) => {};
+const addPartyItem = async (req, res) => {
+  const {
+    partyId,
+    itemName,
+    size,
+    price,
+    sellPrice,
+    quantity,
+    discount,
+    date,
+  } = req.body;
+
+  const { error } = partyItemSchema.validate({
+    itemName,
+    size,
+    price,
+    sellPrice,
+    quantity,
+    discount,
+    date,
+  });
+
+  if (error) {
+    return res.status(400).json({ success: false, message: error.message });
+  }
+
+  try {
+    const newItem = new partyItemModel({
+      partyId,
+      itemName,
+      size,
+      price,
+      sellPrice,
+      quantity,
+      discount,
+      date,
+    });
+
+    const savedItem = await newItem.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Item Added Successfully",
+      partyItem: savedItem,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "server error" });
+  }
+};
 
 //update partyItem
 
